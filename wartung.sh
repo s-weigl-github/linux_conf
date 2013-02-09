fac675f89ed4c30a73c2e3e6b894ffa938685da5
s.weigl.git <s.weigl.web@googlemail.com>
2013-02-09 10:42:34 +0100

    added function
    uptime writes to timeline
    commited out gobby server config(not needed at the moment)

--- wartung.sh
+++ wartung.sh
@@ -7,5 +7,10 @@ date &&
 date +%s &&
 date +%s >> /var/log/wartung_log/timeline.txt &&
 date >> /var/log/wartung_log/timeline.txt &&
+uptime >> /var/log/wartung_log/timeline.txt &&
 echo ------------------------------- >> /var/log/wartung_log/timeline.txt &&
+# echo "starte infinote" &&
+# infinote.sh &&
+# echo " gobby server gestartet" &&
 updatedb
+# echo "alle programme geladen!!!"
