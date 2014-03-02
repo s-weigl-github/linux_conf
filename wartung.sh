8ae677436ee3c57bd166d3598508c513d51af5b6
s.weigl.git <s.weigl.web@googlemail.com>
2014-03-02 11:08:42 +0100

    added little something

--- wartung.sh
+++ wartung.sh
@@ -9,6 +9,10 @@ date +%s >> /var/log/wartung_log/timeline.txt &&
 date >> /var/log/wartung_log/timeline.txt &&
 uptime >> /var/log/wartung_log/timeline.txt &&
 echo ------------------------------- >> /var/log/wartung_log/timeline.txt &&
+awffull &&
+echo awffull updated &&
+webalizer &&
+echo webalizer updated &&
 # echo "starte infinote" &&
 # infinote.sh &&
 # echo " gobby server gestartet" &&
