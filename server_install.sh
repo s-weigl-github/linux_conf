2d117bcaa252a1d0f8869ced016f63651ea67926
s.weigl.git <s.weigl.web@googlemail.com>
2023-01-15 00:53:01 +0100

    Update server_install.sh

--- server_install.sh
+++ server_install.sh
@@ -3,14 +3,9 @@ echo "................................................"
 echo "----starte installation der Web-Server Komponenten----"
 echo "...."
 echo "----Bitte Warten----"
-apt-get install mysql-server mysql-client nginx
-apt-get install php5-common php5 php-pear
-apt-get install php5-cgi php5-mysql php5-curl php5-gd
-apt-get install php5-idn php-pear php5-imagick php5-imap
-apt-get install php5-mcrypt php5-memcache php5-mhash php5-ming
-apt-get install php5-pspell php5-recode php5-snmp php5-sqlite
-apt-get install php5-tidy php5-xmlrpc php5-xsl phpmyadmin
-echo "----zusätzliche pakete----"
-apt-get install 
+apt install apache2 mariadb-server php php-mysql libapache2-mod-php php-xml php-mbstring &&
+apt install php-apcu php-intl imagemagick inkscape php-gd php-cli php-curl php-bcmath git &&
+echo "----apache reload----"
+service apache2 reload
 echo "----installation web-server abgeschlossen----"
 echo "...."
