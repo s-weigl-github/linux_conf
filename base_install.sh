a6e9933e4f3c96aef943961c8ed546f390a55432
s.weigl.git <s.weigl.web@googlemail.com>
2023-09-24 15:26:30 +0200

    Update base_install.sh

--- base_install.sh
+++ base_install.sh
@@ -18,7 +18,7 @@ echo "----systools done----"
 sleep 2
 echo "----info----"
 apt install -yf crazydiskinfo bpytop inxi hddtemp htop neofetch lshw hwinfo
-apt install -yf smartmontools ncdu lsscsi hdparm blktool fzf
+apt install -yf smartmontools ncdu lsscsi hdparm blktool fzf findmnt
 echo "----info done----"
 sleep 2
 echo "----dev----"
