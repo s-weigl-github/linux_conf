9b2385d79e320005d78406cbe335a34b270e7e3c
techadmin <s.weigl.web@googlemail.com>
2025-03-29 10:27:44 +0100

    added script
    cleaning script for bash history file

---
+++ cleanhist.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# little script to clean the history file
+#
+# file to clean
+file=~root/.bash_history
+#$file=hist29032025.txt
+#
+sed -i '/cl/d' $file
+sed -i '/updatedb/d' $file
+sed -i '/mandb/d' $file
+sed -i '/time/d' $file
+sed -i '/ls/d' $file
+sed -i '/nproc/d' $file
